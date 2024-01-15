2341dab9ab5a563e71d0be01e8229a0e7cf89ecc
monamourdev <tiagomendonca.dev@gmail.com>
2024-01-15 19:55:03 -0300

    Upload the project with datas

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# captchacracking
-Pré processamento, extração de características e classificação de números contidos em uma imagem Captcha
+# Captcha Number Recognition
+
+## Pré-processamento, Extração de Características e Classificação de Números.
+![Badge](https://img.shields.io/badge/Octave-v%206.1.1-lightgrey)
+
+<!-- Table of Contents -->
+<details open="open">
+  <summary>Sumário</summary>
+  <ol>
+    <li><a href="#sobre">Sobre</a></li>
+    <li><a href="#status-do-projeto">Status do Projeto</a></li>
+    <li><a href="#modelo">Modelo</a></li>
+    <li><a href="#arquivos">Arquivos</a></li>
+    <li><a href="#testes">Testes</a></li>
+    <li><a href="#tecnologias">Tecnologias</a></li>
+  </ol>
+</details>
+
+# Sobre
+Na etapa de pré-processamento, os dados de entrada passam por diversos métodos que realizam operações como remoção de ruído, segmentação e melhoria da qualidade. Na extração de características, o objetivo é representar os dados de entrada em medidas quantificáveis utilizáveis na etapa de classificação.
+
+# Status do Projeto
+<h4 align="center"> 
+	🚧 ...  Concluído ✅
+</h4>
+
+### Funcionalidades
+
+- [x] Construção do modelo
+- [x] Extração de Momentos de HU
+- [x] Treino e teste
+- [x] Aprendizagem
+- [x] Taxa de acerto
+- [x] Matriz de confusão
+- [x] Teste do classificador
+
+# Modelo
+1. Para cada imagem de números fornecida, foram geradas 19 imagens adicionais com diferentes graus de rotação.
+2. Cada imagem rotacionada foi escalada em sete proporções diferentes: 0.5, 0.75, 1, 1.25, 1.5, 1.75 e 2.
+3. Em cada imagem escalada, foi inserido ruído do tipo sal e pimenta com os seguintes valores: 0, 0.01, 0.02, 0.03 e 0.04.
+
+# Arquivos
+Os arquivos estão localizados na pasta "numbers" dentro do diretório do projeto. Foram utilizadas 10 imagens, uma para cada caractere numérico. 
+Segue uma visualização:
+
+# Testes
+
+# Tecnologias
---
+++ padrao.m
@@ -0,0 +1,80 @@
+function resultado = padrao()
+  pkg load image;
+  endereco='numbers/';
+  arquivos=dir(fullfile(endereco,['*.jpg']));
+  quant_img = length(arquivos);
+  
+  %Queremos criar 19 imagens com rotações diferentes para cada
+  %imagem contida no diretorio
+  qtdeRotacao=19;
+  
+  %Para cada imagem rotacionada, alem das imagens do diretorio,
+  %criei mais 7 imagens com escalas diferentes
+  qtdeEscala=7;
+  
+  %para cada imagem rotacionada, escalada e proveniente do diretorio
+  %criei mais 5 imagens com ruidos sal e pimenta com diferentes intensidades
+  qtdeRuido=5;
+  image_data=cell(length(arquivos),qtdeRotacao*qtdeEscala*qtdeRuido);
+
+  tamanhoImg=.5;
+  ruido=0;
+  passoOriginal= (qtdeEscala*qtdeRuido+qtdeEscala)*qtdeRotacao+qtdeRotacao;
+  passoRotacao = qtdeEscala*qtdeRuido+qtdeEscala;
+  m=1;
+  humom=[];
+  for i=1:length(arquivos)
+    img = imread(fullfile(endereco , arquivos(i,1).name));
+    disp("Loading: "), disp(length(arquivos)-i);
+    image_data{i,1}=img;
+    
+    for j=2:passoRotacao+1:i+passoRotacao*qtdeRotacao
+      num = floor(1 + (360-1)*rand(1,1));
+      image_data{i,j}=imrotate(image_data{i,1}, num);
+      %disp("rotacao: "), disp(j);
+      for k=j+1:qtdeRuido+1:j+passoRotacao
+        image_data{i,k}=imresize(image_data{i,j},tamanhoImg);
+        %disp("escala: "), disp(k);
+        tamanhoImg=tamanhoImg+.25;
+        for l=k+1:k+qtdeRuido
+          image_data{i,l}=imnoise(image_data{i,k},"salt & pepper",ruido);
+          ruido=ruido+.01;
+          humom=[humom;humoments(image_data{i,l})];
+          etiqueta(m)=i-1;
+          m++;
+        endfor
+        ruido=0;
+      endfor
+      tamanhoImg=.5;
+    endfor
+  endfor
+  %separando em conjuntos treino e teste
+  [treino, teste] = amostra(etiqueta,.75);
+  matrizTreino=[];
+  matrizTeste=[];
+  %matriz de treino
+  [tamanhoTreino,~]=size(treino);
+  
+  train_data=humom(treino,:);
+  train_label = etiqueta ( treino );
+  
+  test_data=humom(teste,:);
+  test_label = etiqueta ( teste );
+  
+  
+  model=train_sc(train_data,train_label);
+  % predict eh o conjunto de etiquetas geradas
+  predict=test_sc(model,test_data);
+  
+  %vamos comparar a etiqueta gerada e a original
+  confusionmat(train_label,predict);
+  
+  img = imread("numeros2.jpg");
+  %imgSegmentada eh um vetor cell onde cada celula contem um objeto detectado
+  imgSegmentada= BoundingBoxPatches(img);
+  
+  newMoments=[];
+  for i=1: length(imgSegmentada);
+    newMoments=[nMoments;humoments(imgSegmentada{i})];
+    resultado = test_sc(model,newMoments[i,:]);
+  endfor
